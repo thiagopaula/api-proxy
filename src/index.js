a1fad6eebdbdef6325a6a5651986765ea0b81731
thiagopaula <thiagosampaiopaula@gmail.com>
2025-03-19 23:56:25 -0300

    Update and rename worker.js to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 export default {
   async fetch(request) {
     const url = new URL(request.url);
-    
-    // Modifica a URL para a API da Bybit
+
+    // Modifica a URL para chamar a API da Bybit
     const apiUrl = "https://api.bybit.com" + url.pathname + url.search;
 
-    // Encaminha a requisição para a Bybit
+    // Faz a requisição para a Bybit e retorna a resposta
     const response = await fetch(apiUrl, {
       method: request.method,
       headers: request.headers
     });
 
     return response;
-  }        
+  }
 };
-  
